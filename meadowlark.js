0d79596dd7368834fb3091d752f616826dd72624
Michael Chavez <bigbassroller@gmail.com>
2014-10-20 00:38:35 -0700

    Added formidible

--- meadowlark.js
+++ meadowlark.js
@@ -30,11 +30,15 @@ app.set('port', process.env.PORT || 3000);
 // Where to serve public client side stuff
 app.use(express.static(__dirname + '/public'));
 
+// Parse Forms
+app.use(require('body-parser')());
+
 app.use(function(req, res, next){
 	res.locals.showTests = app.get('env') !== 'production' &&
 	req.query.test === '1';
 	next();
 });
+
 // Dummy function to get current weather data
 function getWeatherData(){
 	return {
@@ -115,9 +119,33 @@ app.get('/data/nursery-rhyme', function(req, res){
 	});
 });
 
+app.get('/newsletter', function(req, res){
+	// we will learn about CSRF later ..for now, we provide dummy value
+	res.render('newsletter', {csrf: "CSRF token goes here"});
+});
+
+app.post('/process', function(req, res){
+	console.log('Form (From querystring): ' + req.query.form);
+	console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+	console.log('Name (from visible form field): ' + req.body.name);
+	console.log('Email (from visible form field): ' + req.body.email);
+	res.render(303, '/thank-you');
+});
+
+app.post('/process', function(req, res){
+	if(reg.xhr || req.accepts('json.html') === 'json'){
+		// if there were an error, we would send {error: 'error description'}
+		res.send({ success: true });
+	} else {
+		// if there were an error , we would redirect to an error page
+		res.redirect(303, '/thank-you');
+	}
+});
+
 app.get('/jquery-test', function(req, res){
 	res.render('jquery-test');
 });
+
 // app.get('/foo', function(req, res){
 // 	res.render('foo', {
 // 		layout: 'microsite'
